df5adf5f5ea596a7351e13d73c01a9c1821b8bc1
Ryan Martin <rjmartin73@gmail.com>
2025-02-25 11:53:20 -0500

    Added API Key

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, HTTPException, File, UploadFile
+from fastapi import FastAPI, HTTPException, File, UploadFile, Header, Request
 from pydantic import BaseModel
 from classify import classify_main
 
 app = FastAPI()
 
+SECRET_API_KEY = "c6eb3706-43fc-41a5-b682-e57dcff5feb0"
 
 @app.get("/")
 async def root():
@@ -11,7 +12,18 @@ async def root():
 
 
 @app.post("/classify")
-async def classify_item(file: UploadFile = File(...)):
+async def classify_item(request: Request, 
+                        file: UploadFile = File(...)
+                        ):
+    
+    headers = dict(request.headers)
+    api_key = request.headers.get("x-api-key")
+    
+    # print("\n📌 Received Headers:", headers, "\n")
+    # print(f"📌 Extracted API Key: {api_key}\n")
+    
+    if api_key != SECRET_API_KEY:
+        raise HTTPException(status_code=403, detail="Unauthorized: Invalid API Key")
     try:
         # Read JSON file
         contents = await file.read()
