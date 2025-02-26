d434a66d7d6c70357313597b53e23a4b2ed19115
Ryan Martin <rjmartin73@gmail.com>
2025-02-26 10:43:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,5 +30,7 @@ async def classify_item(items: List[Item]):
 @app.get("/classification-terms")
 def get_definitions():
     """Returns lists of wire and conduit terms used to classify """
-    return f"Conduit terms: {list(classify.conduit_terms)} \
-            Wire terms: {list(classify.wire_terms)}"
+    return f"Conduit terms: {list(classify.conduit_terms).sort()}\n\
+            Wire terms: {list(classify.wire_terms).sort()}"
+
+# print(get_definitions)
